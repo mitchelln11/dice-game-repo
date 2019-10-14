5394f96fe22cf4dabdfa4250ee363f048576ceb9
mitchelln11 <mitchellnaleid@gmail.com>
2019-10-14 14:48:19 -0500

    init dice game

---
+++ js/dice-three-hundred.js
@@ -0,0 +1,39 @@
+//Get and post player one name
+function playerOne(playerOneName) {
+    document.getElementById("player-one").innerHTML = playerOneName;
+}
+let firstPlayer = playerOne ( prompt("Please Enter Name of Player One") );
+
+//Get and post player one name
+function playerTwo(playerTwoName) {
+    document.getElementById("player-two").innerHTML = playerTwoName;
+}
+let secondPlayer = playerTwo ( prompt("Please Enter Name of Player Two") );
+
+
+// function playerTwo(playerTwoName) {
+//     let playerTwo = document.getElementById("player-two").innerHTML = playerTwoName;
+//     console.log( playerTwo );
+// }
+// let secondPlayer = prompt("Please Enter Name of Player Two");
+// console.log( secondPlayer );
+
+// function rollBull(bullResult){
+//     let diceSixSideValues = [25,25,1,4,7,8];
+//     // let diceSevenSideValues = [];
+//     // let diceEightSideValues = [];
+//     // let diceNineSideValues = [];
+//     // let diceTenSideValues = [];
+//     // let diceElevenSideValues = [];
+
+//     console.log(diceSixSideValues[1]);
+//     // if (){
+
+//     // }
+
+// }
+
+// let origWord = rollBull("hello");
+// console.log(origWord);
+
+// console.log("Hello");
