498fecf2979d1b61207749fcd9e15ab2be939d7d
mitchelln11 <mitchellnaleid@gmail.com>
2019-10-14 16:44:27 -0500

    basic functionality of throwing for bullseye

--- js/dice-three-hundred.js
+++ js/dice-three-hundred.js
@@ -1,37 +1,46 @@
 //Get and post player one name
-function playerOne(playerOneName) {
-    document.getElementById("player-one").innerHTML = playerOneName;
-}
-let firstPlayer = playerOne ( prompt("Please Enter Name of Player One") );
+// function playerOne(playerOneName) {
+//     document.getElementById("player-one").innerHTML = playerOneName;
+// }
+// let firstPlayer = playerOne ( prompt("Please Enter Name of Player One") );
 
-//Get and post player one name
-function playerTwo(playerTwoName) {
-    document.getElementById("player-two").innerHTML = playerTwoName;
-}
-let secondPlayer = playerTwo ( prompt("Please Enter Name of Player Two") );
-
-
+//Get and post player two name
 // function playerTwo(playerTwoName) {
-//     let playerTwo = document.getElementById("player-two").innerHTML = playerTwoName;
-//     console.log( playerTwo );
+//     document.getElementById("player-two").innerHTML = playerTwoName;
 // }
-// let secondPlayer = prompt("Please Enter Name of Player Two");
-// console.log( secondPlayer );
+// let secondPlayer = playerTwo ( prompt("Please Enter Name of Player Two") );
 
-// function rollBull(bullResult){
-//     let diceSixSideValues = [25,25,1,4,7,8];
-//     // let diceSevenSideValues = [];
-//     // let diceEightSideValues = [];
-//     // let diceNineSideValues = [];
-//     // let diceTenSideValues = [];
-//     // let diceElevenSideValues = [];
 
-//     console.log(diceSixSideValues[1]);
-//     // if (){
+let diceSixSideValues = [25,25,1,4,7,8];
+let randomBull = [];
+function rollBull(bullResult){
+    for(let i = 0; i < bullResult.length; i++){
+        randomBull = bullResult[Math.floor(Math.random() * bullResult.length )];
+    }
+    //console.log( randomBull  );
+    if( randomBull == 25 ) {
+        document.getElementById("playerOne15-one").classList.add("green-bg");
+        document.getElementById("shot-message").innerHTML = randomBull + " Hit!";
+    }
+    else {
+        document.getElementById("shot-message").innerHTML = randomBull + " Missed!";
+    }
+}
+rollBull(diceSixSideValues);
+    
+    
+    // console.log(  Math.floor(Math.random(diceSixSideValues) * diceSixSideValues.length )  );
+    // let diceSevenSideValues = [];
+    // let diceEightSideValues = [];
+    // let diceNineSideValues = [];
+    // let diceTenSideValues = [];
+    // let diceElevenSideValues = [];
+    // let bullResult = diceSixSideValues[Math.floor(Math.random() * diceSixSideValues.length)];
+    // let test = diceSixSideValues[1];
+    // console.log( test );
+//     console.log(test);
 
-//     // }
 
-// }
 
 // let origWord = rollBull("hello");
 // console.log(origWord);
